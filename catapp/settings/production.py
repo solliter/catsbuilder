38efc7aeb7940ca45a12e1931b14a0814e3c8c10
solliter <keliran@mail.ru>
2017-08-31 12:42:17 +0300

    Initial Heroku commit

--- catapp/settings/production.py
+++ catapp/settings/production.py
@@ -77,16 +77,11 @@ WSGI_APPLICATION = 'catapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'db_cat',
-    'USER': 'solliter',
-    'PASSWORD': '12345',
-
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
