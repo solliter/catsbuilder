2a96be78db04f49a8a939853bb29633969600e97
solliter <keliran@mail.ru>
2017-08-31 10:51:15 +0300

    Updated

--- catapp/settings/production.py
+++ catapp/settings/production.py
@@ -82,7 +82,7 @@ DATABASES = {
     'NAME': 'db_cat',
     'USER': 'solliter',
     'PASSWORD': '12345',
-    'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+    'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
     'PORT': '3306',
 
 }
